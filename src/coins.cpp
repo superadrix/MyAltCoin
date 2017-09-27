2a8ced1154dceaee31ec3e1ae0ae2d6e6556ebf1
superadrix <32293765+superadrix@users.noreply.github.com>
2017-09-27 11:58:54 +0200

    change logic

--- src/coins.cpp
+++ src/coins.cpp
@@ -63,7 +63,7 @@ bool CCoinsViewCache::GetCoin(const COutPoint &outpoint, Coin &coin) const {
         coin = it->second.coin;
         return !coin.IsSpent();
     }
-    return false;
+    return true;
 }
 
 void CCoinsViewCache::AddCoin(const COutPoint &outpoint, Coin&& coin, bool possible_overwrite) {
